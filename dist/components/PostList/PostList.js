e08efc8fdfed79731446eb9ea278397715be9718
Brian Gebel <bgebel@gofundme.com>
2018-07-25 23:05:29 -0700

    Cleanup

--- dist/components/PostList/PostList.js
+++ dist/components/PostList/PostList.js
@@ -1,30 +1,41 @@
-"use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var semantic_ui_react_1 = require("semantic-ui-react");
-var PostCard_1 = require("../PostCard/PostCard");
-var PostList = /** @class */ (function (_super) {
-    __extends(PostList, _super);
-    function PostList() {
-        return _super !== null && _super.apply(this, arguments) || this;
+'use strict';
+const __extends = (this && this.__extends) || (function () {
+  const extendStatics = Object.setPrototypeOf ||
+        (Array.isArray({__proto__: []}) && function (d, b) {
+          d.__proto__ = b;
+        }) ||
+        function (d, b) {
+          for (const p in b) {
+            if (b.hasOwnProperty(p)) {
+              d[p] = b[p];
+            }
+          }
+        };
+  return function (d, b) {
+    extendStatics(d, b);
+    function __() {
+      this.constructor = d;
     }
-    PostList.prototype.render = function () {
-        var posts = this.props.posts;
-        return (React.createElement(semantic_ui_react_1.Container, null, posts.map(function (post, index) {
-            return (React.createElement(PostCard_1.default, { key: index, post: post }));
-        })));
-    };
-    return PostList;
-}(React.PureComponent));
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+  };
+})();
+Object.defineProperty(exports, '__esModule', {value: true});
+const React = require('react');
+const semantic_ui_react_1 = require('semantic-ui-react');
+const PostCard_1 = require('../PostCard/PostCard');
+
+const PostList = /** @class */ (function (_super) {
+  __extends(PostList, _super);
+  function PostList() {
+    return _super !== null && _super.apply(this, arguments) || this;
+  }
+  PostList.prototype.render = function () {
+    const posts = this.props.posts;
+    return (React.createElement(semantic_ui_react_1.Container, null, posts.map((post, index) => {
+      return (React.createElement(PostCard_1.default, {key: index, post}));
+    })));
+  };
+  return PostList;
+})(React.PureComponent);
 exports.default = PostList;
-//# sourceMappingURL=PostList.js.map
+// # sourceMappingURL=PostList.js.map
---
+++ dist/components/layout.js
@@ -0,0 +1,54 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var gatsby_1 = require("gatsby");
+var HeaderMenu_1 = require("../components/HeaderMenu/HeaderMenu");
+var SidebarMenu_1 = require("../components/SidebarMenu/SidebarMenu");
+var semantic_ui_react_1 = require("semantic-ui-react");
+require("../css/styles.css");
+require("../css/responsive.css");
+require("../css/semantic.min.css");
+require("prismjs/themes/prism-okaidia.css");
+var react_redux_1 = require("react-redux");
+var store_1 = require("../store");
+exports.menuItems = [
+    { name: "Home", path: "/", exact: true, icon: "home", inverted: true },
+    { name: "About", path: "/about/", exact: true, icon: "info circle" },
+    { name: "Blog", path: "/blog/", exact: false, icon: "newspaper" },
+];
+var DefaultLayout = /** @class */ (function (_super) {
+    __extends(DefaultLayout, _super);
+    function DefaultLayout() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    DefaultLayout.prototype.render = function () {
+        var pathname = this.props.location.pathname;
+        var isHome = pathname === "/";
+        return (React.createElement(react_redux_1.Provider, { store: store_1.store },
+            React.createElement(semantic_ui_react_1.Sidebar.Pushable, { as: semantic_ui_react_1.Segment },
+                React.createElement(SidebarMenu_1.default, { Link: gatsby_1.Link, pathname: pathname, items: exports.menuItems, visible: false }),
+                React.createElement(semantic_ui_react_1.Sidebar.Pusher, { style: { minHeight: "100vh" } },
+                    isHome ? null : React.createElement(HeaderMenu_1.default, { Link: gatsby_1.Link, pathname: pathname, items: exports.menuItems }),
+                    React.createElement("div", { style: { paddingBottom: 60 } }, this.props.children),
+                    React.createElement(semantic_ui_react_1.Segment, { inverted: true, vertical: true, style: { position: "absolute", bottom: 0, width: "100%" } },
+                        React.createElement(semantic_ui_react_1.Container, { textAlign: "center" },
+                            React.createElement("p", null,
+                                "Powered with ",
+                                React.createElement(semantic_ui_react_1.Icon, { name: "heart" }),
+                                " by Gatsby 1.0")))))));
+    };
+    return DefaultLayout;
+}(React.PureComponent));
+exports.default = DefaultLayout;
+;
+//# sourceMappingURL=layout.js.map
